6eaecbbc90c9489a3cee195d1ba0ccae95122b59
Jerry B. Lahu <116436001+MrSakusei@users.noreply.github.com>
2024-01-16 03:26:29 -0800

    Update BookService.java

--- book/src/main/java/com/book/book/service/BookService.java
+++ book/src/main/java/com/book/book/service/BookService.java
@@ -1,1 +1,17 @@
+package com.book.book.service;
+
+import com.book.book.domain.Book;
+
+import java.util.List;
+
+public interface BookService
+{
+    Book saveBook(Book book);
+    List<Book> saveBooks(List<Book> books);
+    Book getBookById(long id);
+    List<Book> getAllBooks();
+    Book updateBook(Book book);
+    void deleteBookById(long id);
+    void deleteBooks();
+}
 
