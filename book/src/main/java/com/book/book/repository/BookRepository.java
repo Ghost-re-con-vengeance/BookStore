94276905bf7c82737cfca43d0b0c42a69f0ecda6
Esosa013 <132652896+Esosa013@users.noreply.github.com>
2024-01-16 02:54:27 -0800

    Update BookRepository.java

--- book/src/main/java/com/book/book/repository/BookRepository.java
+++ book/src/main/java/com/book/book/repository/BookRepository.java
@@ -1,9 +1,1 @@
-package com.book.book.repository;
 
-import com.book.book.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
